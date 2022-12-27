3ce8272fa15f3a034431f4683c6e064efa10ad90
Sebastian <sebastian.stephenson1@gmail.com>
2022-12-27 17:04:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(600, 600);
+    background(153);
+
+    fill(50, 50, 50) ;
+    rect(590, 100, 10, 140);
+
+    fill(200, 0, 0);
+    rect(540, 200, 50, 10);
+    
+  }
+
+  function draw(){
+    
+  }
+
+  function gameStart(){
+    
+  }
